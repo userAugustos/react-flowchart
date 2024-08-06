240a31923c06265593fa6e2543c3399bb36c8b9e
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-08-06 12:07:18 -0300

    feat: able to set label text to edges

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,183 @@
-import { useState, useCallback } from 'react';
+import {useCallback, useMemo, useState} from 'react';
 import {
-  ReactFlow,
-  Controls,
-  Background,
-  applyNodeChanges,
+  addEdge,
   applyEdgeChanges,
-  addEdge, BackgroundVariant,
+  applyNodeChanges,
+  Background,
+  BackgroundVariant, BaseEdge,
+  Connection,
+  Controls,
+  Edge,
+  EdgeChange, EdgeLabelRenderer, EdgeText, getStraightPath, GetStraightPathParams,
+  Handle, MarkerType,
+  Node,
+  NodeChange, Panel,
+  Position,
+  ReactFlow, useReactFlow,
 } from '@xyflow/react';
-import '@xyflow/react/dist/style.css';
+import {useDebounce, useDebouncedCallback} from "use-debounce";
 
-const initialNodes = [
-  {
-    id: '1',
-    style: { color: '#000' },
-    data: { label: 'Hello' },
-    position: { x: 0, y: 0 },
-    type: 'input',
-  },
-  {
-    id: '2',
-    style: { color: '#000' },
-    data: { label: 'World' },
-    position: { x: 100, y: 100 },
-  },
-];
+const initialNodes: Node[] = [];
 
-const initialEdges = [];
+const initialEdges: Edge[] = [];
+
+const Circle = () => {
+  return <div className="circle">
+    <Handle type="target" position={Position.Top} />
+    <Handle type="source" position={Position.Bottom} />
+  </div>
+}
+
+const Diamond = () => {
+  return <div className="diamond">
+    <Handle type="source" position={Position.Bottom}/>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" className="bi bi-diamond" viewBox="0 0 16 16"
+         id="Diamond--Streamline-Bootstrap" height={16} width={16}>
+      <desc>{"Diamond Streamline Icon: https://streamlinehq.com"}</desc>
+      <path
+        d="M6.95 0.435c0.58 -0.58 1.52 -0.58 2.1 0l6.515 6.516c0.58 0.58 0.58 1.519 0 2.098L9.05 15.565c-0.58 0.58 -1.519 0.58 -2.098 0L0.435 9.05a1.48 1.48 0 0 1 0 -2.098zm1.4 0.7a0.495 0.495 0 0 0 -0.7 0L1.134 7.65a0.495 0.495 0 0 0 0 0.7l6.516 6.516a0.495 0.495 0 0 0 0.7 0l6.516 -6.516a0.495 0.495 0 0 0 0 -0.7L8.35 1.134z"
+        strokeWidth={1}/>
+    </svg>
+    <Handle type="target" position={Position.Top}/>
+  </div>
+}
+
+const TextEdge = ({ sourceX, sourceY, targetX, targetY, id, data, selected }: GetStraightPathParams & {id: string, data: Record<string, any>, selected: boolean}) => {
+  const { updateEdge } = useReactFlow();
+  const [edgePath, labelX, labelY] = getStraightPath({
+    sourceX,
+    sourceY,
+    targetX,
+    targetY,
+  });
+  const [text, setText] = useState('');
+  console.debug('data',data)
+
+  const debounced = useDebouncedCallback<(value: string) => void>((value) => {
+    updateEdge(id, {
+      data: { label: value }
+    })
+  }, 1000)
+
+  const handleChangeLabel = (e:  React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+    debounced(e.target.value)
+  }
+
+  return <>
+    <BaseEdge id={id} path={edgePath} label="fdsf"/>
+    <EdgeLabelRenderer>
+      <input type="text" className="edge-input" value={text} onChange={handleChangeLabel}
+             style={{transform: `translate(-50%, -200%) translate(${labelX}px,${labelY}px)`, display: `${selected ? 'block' : 'none'}` }}/>
+    </EdgeLabelRenderer>
+    <EdgeText
+      x={labelX}
+      y={labelY}
+      label={text}
+      labelStyle={{fill: 'white'}}
+      labelShowBg
+      labelBgStyle={{fill: 'red'}}
+      labelBgPadding={[2, 4]}
+      labelBgBorderRadius={2}
+    />
+  </>
+}
+
+function generateShapeId(arr: Node[]) {
+  if (arr.length === 0) return '1'
+
+  return String(Number(arr[arr.length - 1].id) + 1);
+}
 
 function Flow() {
-  const [nodes, setNodes] = useState(initialNodes);
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
 
+  const nodeTypes = useMemo(() => ({circle: Circle, diamond: Diamond}), [])
+  const edgeTypes = useMemo(() => ({ text: TextEdge }), [])
+
+  const exportFlowChart = () => {
+    console.debug([
+      ...nodes,
+      ...edges
+    ])
+
+    const fileData = JSON.stringify({
+      shapes: nodes,
+      edges: edges
+    })
+
+    const blob = new Blob([fileData], {type: 'text/plain'})
+
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.download = "aira.drawio";
+
+    link.href = url;
+    link.click();
+  }
+
+  const handleAddDiamond = () => {
+    setNodes(prev => ([...prev, {
+      id: generateShapeId(prev),
+      style: { color: '#000' },
+      data: {  },
+      position: { x: 150, y: 100 },
+      type: 'diamond'
+    }]))
+  }
+
+  const handleAddCircle = () => {
+    setNodes(prev => ([...prev, {
+      id: generateShapeId(prev),
+      style: { color: '#000' },
+      data: { label: 'circle' },
+      position: { x: 150, y: 100 },
+      type: 'circle'
+    }]))
+  }
+
+  const handleAddSquare = () => {
+    setNodes(prev => ([...prev, {
+      id: generateShapeId(prev),
+      style: { color: '#000' },
+      data: { },
+      position: { x: 150, y: 100 },
+    }]))
+  }
+
   const onNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    (changes:  NodeChange<Node>[]) => {
+      console.debug(changes)
+      setNodes((nds) => applyNodeChanges(changes, nds))
+    },
     [],
   );
   const onEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    (changes: EdgeChange<Edge>[]) => {
+      console.debug(changes)
+      setEdges((eds) => applyEdgeChanges(changes, eds.map(edg => ({
+        ...edg, markerEnd: { type: MarkerType.Arrow }, type: 'text', data: { label: 'test' } }
+      ))))
+    },
     [],
   );
 
   const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [],
+    (connection: Connection) => {
+      // const edge = { ...connection, type: 'text' };
+      const edge = { ...connection };
+      setEdges((eds) => addEdge(edge, eds));
+    },
+    [setEdges],
   );
 
   return (
     <div className="container">
       <ReactFlow
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
         nodes={nodes}
         onNodesChange={onNodesChange}
         edges={edges}
@@ -57,9 +187,47 @@ function Flow() {
       >
         <Background variant={BackgroundVariant.Lines} />
         <Controls />
+
+        <Panel>
+          <button className="export-btn" onClick={exportFlowChart}>
+            Export
+          </button>
+        </Panel>
+        <ShapesPanel handleAddDiamond={handleAddDiamond} handleAddCircle={handleAddCircle} handleAddSquare={handleAddSquare} />
       </ReactFlow>
     </div>
   );
+}
+
+
+const ShapesPanel = ({ handleAddDiamond, handleAddSquare, handleAddCircle }: any) => {
+  return <Panel position="top-center" className="panel-horizontal">
+    <button className="shapes-icons" onClick={handleAddDiamond}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" className="bi bi-diamond" viewBox="0 0 16 16"
+           id="Diamond--Streamline-Bootstrap" height={16} width={16}>
+        <desc>{"Diamond Streamline Icon: https://streamlinehq.com"}</desc>
+        <path
+          d="M6.95 0.435c0.58 -0.58 1.52 -0.58 2.1 0l6.515 6.516c0.58 0.58 0.58 1.519 0 2.098L9.05 15.565c-0.58 0.58 -1.519 0.58 -2.098 0L0.435 9.05a1.48 1.48 0 0 1 0 -2.098zm1.4 0.7a0.495 0.495 0 0 0 -0.7 0L1.134 7.65a0.495 0.495 0 0 0 0 0.7l6.516 6.516a0.495 0.495 0 0 0 0.7 0l6.516 -6.516a0.495 0.495 0 0 0 0 -0.7L8.35 1.134z"
+          strokeWidth={1}/>
+      </svg>
+    </button>
+    <button className="shapes-icons" onClick={handleAddSquare}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" className="bi bi-square" viewBox="0 0 16 16"
+           id="Square--Streamline-Bootstrap" height={16} width={16}>
+        <desc>{"Square Streamline Icon: https://streamlinehq.com"}</desc>
+        <path
+          d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1 -1 1H2a1 1 0 0 1 -1 -1V2a1 1 0 0 1 1 -1zM2 0a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2V2a2 2 0 0 0 -2 -2z"
+          strokeWidth={1}/>
+      </svg>
+    </button>
+    <button className="shapes-icons" onClick={handleAddCircle}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" className="bi bi-circle" viewBox="0 0 16 16"
+           id="Circle--Streamline-Bootstrap" height={16} width={16}>
+        <desc>{"Circle Streamline Icon: https://streamlinehq.com"}</desc>
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" strokeWidth={1}/>
+      </svg>
+    </button>
+  </Panel>
 }
 
 export default Flow;
